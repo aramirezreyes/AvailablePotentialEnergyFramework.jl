fbf99a3e4b1912bb970c3c96126f01c228f54713
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2020-06-13 19:04:39 -0700

    first try to register

---
+++ src/arraytools.jl
@@ -0,0 +1,6 @@
+using 
+function detect_cyclone(array,threshold)
+    surf_pres_filtered = smoothfilter(array,threshold);
+    peaks              = findlocalmaxima(surf_pres_filtered);
+    return  seeded_region_growing(array, seeds)
+end
