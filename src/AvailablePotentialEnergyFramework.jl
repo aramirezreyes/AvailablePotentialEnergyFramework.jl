4d577ed44c6f8c1777d7ad675ce1254ec603c719
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2020-06-21 08:12:46 -0700

    Change docstring for the module

--- src/AvailablePotentialEnergyFramework.jl
+++ src/AvailablePotentialEnergyFramework.jl
@@ -1,5 +1,11 @@
 VERSION < v"0.1" && __precompile__()
 
+"""
+Data filters:    `filter_array`, `filter_array_2`, `filter_array_time`, `getsmoothdata`, `getsmoothdata_nospace`
+Data structures:  `ape_budget`, `cat_ape_budget`, `cutborders!`, `surf_quantities`, `cyclone_comp_timemean`, `Composite_Cyclone`, `Composite_Cyclone_v2`, `Composite_Cyclone_v3`
+Methods:    `cyclonecompositer`, `shifter`, `smoothfilter`, `cyclonecompositer_v2`, `cyclonecompositer_v3`, `timemean_nofalseframe`, `removefalseframes`, `getapebudget`, `buoyancybudget`
+
+"""
 module AvailablePotentialEnergyFramework
 
 using ImageFiltering,
@@ -61,13 +67,5 @@ export
     get_divergence,
     #math functions
     integrate_vertically
-"""
-Data filters:    `filter_array`, `filter_array_2`, `filter_array_time`, `getsmoothdata`, `getsmoothdata_nospace`
-Data structures:  `ape_budget`, `cat_ape_budget`, `cutborders!`, `surf_quantities`, `cyclone_comp_timemean`, `Composite_Cyclone`, `Composite_Cyclone_v2`, `Composite_Cyclone_v3`
-Methods:    `cyclonecompositer`, `shifter`, `smoothfilter`, `cyclonecompositer_v2`, `cyclonecompositer_v3`, `timemean_nofalseframe`, `removefalseframes`, `getapebudget`, `buoyancybudget`
-
-"""
     
-SAMtools
-
 end
