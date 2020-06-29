f0fe7efba09c762c8ea836ca252f16e54587e304
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2020-06-28 18:40:13 -0700

    Added functions to detect better cyclones and use image segmentation

--- src/compositehelperfunctions.jl
+++ src/compositehelperfunctions.jl
@@ -1,19 +1,103 @@
-import Images: findlocalminima
+using Images: findlocalminima
+using ImageSegmentation: SegmentedImage, segment_labels, region_adjacency_graph
+using SparseArrays: SparseMatrixCSC
+
+"""
+   azimuthalaverage(buf,array,segmentedcyclones,segmentlabels,gridspacing)
+
+
+"""
+function azimuthalaverage!(buf1,buf2,array,segmentedcyclones,cyclonescenters,gridspacing)
+    G, vert_map = region_adjacency_graph(segmentedcyclones, (i,j)->1)
+    labelsmap = labels_map(segmentedcyclones)
+    adjacencymatrix = G.weights
+    for cyclone in 1:(length(segmentedcyclones.segment_labels)-1)
+        if !isinteracting(adjacencymatrix,cyclone)
+            shifter!(buf1,array,size(array).÷2,cyclonescenters[cyclone][1])
+            shifter!(buf2,labelsmap,size(array).÷2,cyclonescenters[cyclone][1])
+            @info "Added 1 cyclone"
+        end
+    end
+end
+
+"""
+    neighbours_2d(arraysize,indicesCartesian)
+Compute de neighboring points of an index considering periodic boundaries
+"""
+function neighbours_2d(arraysize,indicesCartesian)
+    decrement_mod1(num, n) = num == 1 ? n : num - 1
+    increment_mod1(num, n) = num == n ? one(num) : num + 1
+    x_ind = indicesCartesian[1]
+    y_ind = indicesCartesian[2]
+    size_x = arraysize[1]
+    size_y = arraysize[2]
+    @inbounds neighbours = Iterators.product(
+        (decrement_mod1(x_ind,size_x), x_ind, increment_mod1(x_ind,size_x)),
+        (decrement_mod1(y_ind,size_y), y_ind, increment_mod1(y_ind,size_y)) )
+    return CartesianIndex.(neighbours)
+end
+
+"""
+    function detect_cyclones(surface_pressure,pressure_threshold,resolution)
+
+"""
+function detect_cyclones(surface_pressure,pressure_threshold,resolution)
+    neighbourhood_gen(arraysize) = point -> AvailablePotentialEnergyFramework.neighbours_2d(arraysize,point)
+    pres_anomaly = surface_pressure .- mean(surface_pressure,dims=(1,2))
+    centers_and_labels = findcyclonecenters_aspressureminima(pres_anomaly,pressure_threshold,resolution)
+    centers_and_labels = [(centers_and_labels[i],i) for i in 1:length(centers_and_labels)]
+    mask = pres_anomaly .<= pressure_threshold
+    push!(centers_and_labels,(findfirst(!,mask),1000))
+    cyclones = seeded_region_growing(pres_anomaly.*mask,centers_and_labels,neighbourhood_gen(size(surface_pressure)))
+    return (centers_and_labels,cyclones)
+end
+
+"""
+    isinteracting(adjacencyMatrix :: SparseMatrixCSC,regionnumber)
+Computes the adjacency of identified cyclones and returns true if the cylone-number is adjacent to a region other than the background.
+"""
+function isinteracting(adjacencyMatrix :: SparseMatrixCSC,regionnumber)
+    @inbounds for i in 1:(adjacencyMatrix.m - 1)
+#        @views adjacencyMatrix[regionnumber,i] == 1 ? (return true) : nothing
+        @views adjacencyMatrix[regionnumber,i] == 1  && (return true) 
+    end
+    return false
+end
+
+"""
+    isinteracting(cyclones :: SegmentedImage,regionnumber)
+Computes the adjacency of identified cyclones and returns true if the cylone-number is adjacent to a region other than the background.
+"""
+function isinteracting(cyclones :: SegmentedImage,regionnumber)
+    adjacency, vert_map = region_adjacency_graph(cyclones,(i,j)->1)
+    @inbounds for i in 1:(adjacency.weights.m - 1)
+        adjacency.weights[regionnumber,i] == 1 ? (return true) : nothing
+    end
+    return false
+end
+
 """
     findcyclonecenters_aspressureminima(surf_pres_anomaly,detection_threshold)
+
 Takes a surface pressure anomaly array surf_pres_anomaly[x,y] = surf_pres[x,y] .- mean(surf_pres,dims(1,2))
 a detection threshold for the anomaly, and return an array of tuples (x,y) 
 where each tuple represents the centers of cyclones identified as the minima of the anomaly.
 """
-function findcyclonecenters_aspressureminima(surf_pres_anomaly,detection_threshold)
-    surf_pres_filtered = smoothfilter(surf_pres_anomaly,detection_threshold);
-    peaks              = findlocalminima(surf_pres_filtered);
+function findcyclonecenters_aspressureminima(surf_pres_anomaly,detection_threshold,resolution)
+    surf_pres_filtered = smoothfilter(surf_pres_anomaly,detection_threshold,resolution);
+    peaks              = findlocalminima(surf_pres_filtered,[1,2],false);
 end
 
+"""
+    smoothfilter(surf_pres_anomaly, threshold = 9)
 
-
-function smoothfilter(surf_pres_anomaly,treshold=9)
-    surf_pres_median = mapwindow(median!,surf_pres_anomaly,[21,21]);
+Takes a 2d array and smooths it using a rolling median filter. 
+It then returns the elements of the filtered array whose values are less than the threshold.
+"""
+function smoothfilter(surf_pres_anomaly,treshold=9, resolution = 2000)
+    windowsize = 30000 ÷ (2*resolution) # Length order of magnitude of the eye
+    windowsize % 2 == 0 ? windowsize = windowsize + 1 : windowsize
+    surf_pres_median = mapwindow(median!,surf_pres_anomaly,[windowsize,windowsize]);
     surf_pres_median = surf_pres_median.*(surf_pres_median.<treshold);
     surf_pres_filtered = imfilter(surf_pres_median,Kernel.gaussian(3));
     surf_pres_filtered = surf_pres_filtered.*(surf_pres_filtered.<treshold);
@@ -43,11 +127,13 @@ Receives and SAM 3D+time or 2D+time array and two tuples, the (x,y) indices of t
 and the (x,y) indices of the location of the pressure perturbation peak.
 """
 function shifter!(dest,array,domain_center,peak)
-    if ndims(array)==3
-      return  circshift!(dest,array,[domain_center[1]-peak[1],domain_center[2]-peak[2]]);
-    elseif ndims(array)==4
-      return  circshift!(dest,array,[domain_center[1]-peak[1],domain_center[2]-peak[2],0]);
-    end
+#    if 
+        return  circshift!(dest,array,[domain_center[1]-peak[1],domain_center[2]-peak[2]]);
+#    elseif ndims(array)==3
+    #     return  circshift!(dest,array,[domain_center[1]-peak[1],domain_center[2]-peak[2]]);
+    # elseif ndims(array)==4
+    #   return  circshift!(dest,array,[domain_center[1]-peak[1],domain_center[2]-peak[2],0]);
+#    end
 end
 
 mutable struct Composite_Cyclone
@@ -125,29 +211,36 @@ end
 #         end
 #         return smooth_array(copy[:,:,:,1:trueframe])
 # end
-    
+"""
+    timemean_nofalseframe(input)
+
+Takes a 3-d or 3-d array and computes the average along the third or fourth dimension, skipping the slices input[:,:,:,i] for which the maxiumum value is 0.0
+
+"""
 function timemean_nofalseframe(input::Array{T,4}) where {T<:Real}
-        copy = zeros(size(input))
+        sx,sy,sz,st = size(input)
+        copy = zeros(T,sx,sy,sz)
         trueframes = 1
         for t in 1:size(input,4)
-            if maximum(view(input,:,:,:,t)) != 0
-               copy[:,:,:,trueframes] = input[:,:,:,t]
+            if maximum(view(input,:,:,:,t)) != 0.0
+               copy[:,:,:] .+= @views  input[:,:,:,t]
                trueframes = trueframes + 1
             end
         end
-        return mean(copy[:,:,:,1:trueframes-1],dims=4)
+        return copy .= copy ./ trueframes
 end
     
 function timemean_nofalseframe(input::Array{T,3}) where {T<:Real}
-        copy = zeros(size(input))
+        sx,sy,st = size(input)
+        copy = zeros(T,sx,sy)
         trueframes = 1
-        for t in 1:size(input,3)
-            if maximum(view(input,:,:,t)) != 0
-               copy[:,:,trueframes] = input[:,:,t]
+        for t in 1:size(input,4)
+            if maximum(view(input,:,:,t)) != 0.0
+               copy[:,:] .+= @views  input[:,:,t]
                trueframes = trueframes + 1
             end
         end
-        return mean(copy[:,:,1:trueframes-1],dims=3)
+        return copy .= copy ./ trueframes
 end
     
 function removefalseframes(input::Array{T,4},peaktimes) where {T<:Real}
