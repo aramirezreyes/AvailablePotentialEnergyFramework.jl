84784930bd84560f45401a59f7351a0dca71a396
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2020-08-19 19:16:51 -0700

    Added docstring for function that smooths and writes smoothed files to netcdf

--- src/datamanagement.jl
+++ src/datamanagement.jl
@@ -1,6 +1,10 @@
 using NCDatasets: Dataset
 using DataStructures: OrderedDict
 
+"""
+    smooth_vars_and_write_to_netcdf!(output_file,input_file,vars_to_smooth,window_h,window_t)
+Take a netcdf file, a list of variables and two integers and performs a moving mean smoothing of these variables. Write the smoothed fields into a netcdf file called output_file
+"""
 function smooth_vars_and_write_to_netcdf!(output_file,input_file,vars_to_smooth,window_h,window_t)
 
     ds_orig = Dataset(input_file)
