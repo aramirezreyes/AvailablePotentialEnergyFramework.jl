436dc95923f202bb56aa77643b29c5512c3ec14e
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2020-09-03 14:24:45 -0500

    added tests for N2, not working

---
+++ test/physicsfunctions.jl
@@ -0,0 +1,15 @@
+@testset Brunt-Vaisala
+Γneutral = AvailablePotentialEnergyFramework.g/AvailablePotentialEnergyFramework.dryair.cp
+Γstable = Γneutral + 5
+Γunstable = Γneutral - 50
+
+z = collect(0:50:10e3)
+tvprofile(Γ,z) = reshape(repeat(300 .- Γ*z./1000,1,2),1,1,length(z),2)
+
+@test compute_N2(tvprofile(Γneutral,z),z) ≈ zeros(length(z),2)
+
+@test all(compute_N2(tvprofile(Γunstable,z),z) .< 0 )
+
+@test all(compute_N2(tvprofile(Γstable,z),z) .> 0 )
+
+
