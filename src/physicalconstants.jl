c8ccb396f2063761e148efdb25b30bde61086e62
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-03-22 10:38:13 -0700

    Made substances parametric and constants

--- src/physicalconstants.jl
+++ src/physicalconstants.jl
@@ -1,27 +1,29 @@
+
 const epsilon       = 18.016/28.966
 const g             = 10u"m/s/s" #acceleration of gravity
 
-@with_kw struct Substance
-    cp = nothing
-    cv = nothing
-    R = nothing
-    Lv = nothing
-    Lf = nothing
+struct Substance{T} 
+    cp :: Union{Nothing,T}
+    cv :: Union{Nothing,T}
+    R  :: Union{Nothing,T}
+    Lv :: Union{Nothing,T}
+    Lf :: Union{Nothing,T}
 end
 
+Substance{T}(;cp = nothing, cv = nothing, R = nothing, Lv = nothing, Lf = nothing) where T = Substance{T}(cp,cv,R,Lv,Lf) 
 
-Dryair = Substance(
-    cp = 1006u"J/kg/K" ,#J/kg/k at 1013 hPa
-    cv = 718u"J/kg/K",
+const Dryair = Substance{Quantity}(
+    cp = 1006.0u"J/kg/K", #J/kg/k at 1013 hPa
+    cv = 718.0u"J/kg/K",
     R  = 287.05u"J/kg/K" # J/kg/k
 )
 
-Liquidwater = Substance(
+const Liquidwater = Substance{Quantity}(
      Lv = 2.5e6u"J/kg", #J/kg
      Lf = 3.33e5u"J/kg",
-     cp = 4200u"J/kg/K" #j/kg/k
+     cp = 4200.0u"J/kg/K" #j/kg/k
 )
 
-Watervapor = Substance(
+const Watervapor = Substance{Quantity}(
     R = 461.52u"J/kg/K" #j/kg/K
 )
