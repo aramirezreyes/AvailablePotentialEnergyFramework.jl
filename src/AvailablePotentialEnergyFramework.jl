067dc4cb48587d0f660b4c850efe72e5347fa4e9
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2021-04-25 03:07:55 -0700

    Removed type.jl

--- src/AvailablePotentialEnergyFramework.jl
+++ src/AvailablePotentialEnergyFramework.jl
@@ -22,7 +22,6 @@ using Unitful: @u_str, unit, ustrip, Quantity
 using LoopVectorization: @avx
 using Tullio: @tullio
 
-include("types.jl")
 include("apehelperfunctions.jl")
 include("compositehelperfunctions.jl")
 include("apebudgets.jl")
